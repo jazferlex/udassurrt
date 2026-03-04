5444612fa684b6487ee2b4dd9263c3cb17a783fa
Jazfer Licaros <jazferlex@gmail.com>
2026-03-04 11:36:37 +0800

    Refactor card styles and layout for categories and products pages

--- app/components/catalog/page.tsx
+++ app/components/catalog/page.tsx
@@ -4,7 +4,7 @@ import Products from "./products/page";
 export default function Catalog() {
   
   return (
-    <div className="min-h-screen bg-background flex flex-row">
+    <div className="min-h-screen bg-background flex flex-row gap-4">
       <Categories />
       <Products />
     </div>
